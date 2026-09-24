5df5416f432489d6704648383c45d4af48b24564
jvanijcken <j.vanijcken@gmail.com>
2026-09-24 10:36:29 +0200

    remove hacky pynndescent optimization

--- src/cli.py
+++ src/cli.py
@@ -1,14 +1,6 @@
 import argparse
 from pathlib import Path
-import time
 
-OPTIMIZED_MODE = True
-
-if OPTIMIZED_MODE:  # super hacky method to save on import time
-    import sys
-    from unittest.mock import MagicMock
-
-    sys.modules['pynndescent'] = MagicMock()  # saves import time
 
 DEFAULT_CONFIG_DIR = Path("../config")
 DEFAULT_MN_CONFIG = DEFAULT_CONFIG_DIR / "mn.yaml"
