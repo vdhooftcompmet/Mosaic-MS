2bd363a87ad4bd27f6a94c3a9fc327991f27cabb
jvanijcken <j.vanijcken@gmail.com>
2026-09-24 12:22:28 +0200

    remove unneeded feature

--- src/add_snapms/main.py
+++ src/add_snapms/main.py
@@ -19,15 +19,12 @@ def main(params):
     annotation_dict = dict(zip(cluster_ids, annotations))
     add_snapms_data(mn, annotation_dict)
 
-    annotation_dict = dict(zip(cluster_ids, annotations))
-    set_smiles_flag(mn, annotation_dict)
-
     write_cx(mn, str(params.graph), MN_STYLE_FILE)
     for file, annotation in zip(files, annotations):
         write_cx(annotation, file, ANNOTATION_STYLE_FILE)
 
 
-def add_snapms_data(mn: nx.Graph, annotations: dict[str | int, nx.Graph]) -> None:
+def add_snapms_data(mn: nx.Graph, annotations: dict) -> None:
     clusters = defaultdict(set)
 
     for node in mn:
@@ -55,30 +52,3 @@ def add_snapms_data(mn: nx.Graph, annotations: dict[str | int, nx.Graph]) -> Non
         for node in clusters[cluster_id]:
             mn.nodes[node][KEY_IS_ANNOTATED] = is_top_candidate
 
-
-def set_smiles_flag(network: nx.Graph, annotations: dict[int | nx.Graph]) -> None:
-    
-    clusters = defaultdict(list)
-
-    for node in network:
-        cluster = network.nodes[node][KEY_MN_CLUSTER_ID]
-        clusters[cluster].append( node )
-
-    for cluster_index, annotation in annotations.items():
-        cluster = clusters.get(cluster_index, [])
-
-        valid_cluster_nodes     = [n for n in cluster    if network.nodes[n].get(KEY_SMILES)]
-        valid_annotation_nodes  = [n for n in annotation if annotation.nodes[n].get(KEY_SMILES)]
-
-        cluster_smiles      = [network.nodes[n].get(KEY_SMILES)    for n in valid_cluster_nodes]
-        annotation_smiles   = [annotation.nodes[n].get(KEY_SMILES) for n in valid_annotation_nodes]
-
-        if (not cluster_smiles) or (not annotation_smiles):
-            continue
-
-        similarity = similarity_matrix(cluster_smiles, annotation_smiles, "morgan", "dice")
-
-        for i, node in enumerate(valid_annotation_nodes):
-            matches = similarity[:, i]
-            is_match = bool( np.max(matches) >= 0.66 )
-            annotation.nodes[node][KEY_HAS_SMILES_MATCH] = is_match
